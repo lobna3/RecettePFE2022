9c5007fee1787147e8ad3000c882386afe637ff5
lobna3 <yousfi55lobna@gmail.com>
2022-05-12 18:10:50 +0100

    message

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -1,1 +1,357 @@
-
+<body style="font-family:Tahoma;font-size:12px;color: #333333;background-color:#FFFFFF;">
+  <table width="100%" height="100" cellspacing="0" cellpadding="0">
+    <tbody>
+      <tr>
+        <td>
+          <div align="center" style="font-size: 14px;font-weight: bold;">
+            Facture № 553{" "}
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <table
+    width="100%"
+    cellspacing="0"
+    cellpadding="2"
+    border="1"
+    bordercolor="#CCCCCC"
+  >
+    <tbody>
+      <tr>
+        <td
+          width="35%"
+          bordercolor="#ccc"
+          bgcolor="#f2f2f2"
+          style="font-size:12px;"
+        >
+          <strong>Désignation </strong>
+        </td>
+        <td bordercolor="#ccc" bgcolor="#f2f2f2" style="font-size:12px;">
+          <strong>Quantité</strong>
+        </td>
+        <td bordercolor="#ccc" bgcolor="#f2f2f2" style="font-size:12px;">
+          <strong>Prix</strong>
+        </td>
+        <td bordercolor="#ccc" bgcolor="#f2f2f2" style="font-size:12px;">
+          <strong>Total</strong>
+        </td>
+      </tr>
+      <tr style="display:none;">
+        <td colspan="*"></td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          mon produit
+        </td>
+        <td valign="top" style="font-size:12px;">
+          3{" "}
+        </td>
+        <td valign="top" style="font-size:12px;">
+          60,00{" "}
+        </td>
+        <td valign="top" style="font-size:12px;">
+          180,00{" "}
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          mon service
+        </td>
+        <td valign="top" style="font-size:12px;">
+          1{" "}
+        </td>
+        <td valign="top" style="font-size:12px;">
+          120,00{" "}
+        </td>
+        <td valign="top" style="font-size:12px;">
+          120,00{" "}
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+        <td valign="top" style="font-size:12px;">
+          &nbsp;
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <table width="100%" cellspacing="0" cellpadding="2" border="0">
+    <tbody>
+      <tr>
+        <td style="font-size:12px;width:50%;">
+          <strong> </strong>
+        </td>
+        <td>
+          <table width="100%" cellspacing="0" cellpadding="2" border="0">
+            <tbody>
+              <tr>
+                <td align="right" style="font-size:12px;">
+                  Total
+                </td>
+                <td align="right" style="font-size:12px;">
+                  300,00 €
+                </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td align="right" style="font-size:12px;">
+                  <b>Total TTC</b>
+                </td>
+                <td align="right" style="font-size:12px;">
+                  <b>300,00 €</b>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>;
